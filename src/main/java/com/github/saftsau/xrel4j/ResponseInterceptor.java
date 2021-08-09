ebb6fc6bbe1f3337be460e463a4a9fe4eefa1ea3
Paul Hagedorn <panzer1119@gmail.com>
2021-08-09 20:30:41 +0200

    style: format ResponseInterceptor.java

--- src/main/java/com/github/saftsau/xrel4j/ResponseInterceptor.java
+++ src/main/java/com/github/saftsau/xrel4j/ResponseInterceptor.java
@@ -18,155 +18,162 @@
 package com.github.saftsau.xrel4j;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import okhttp3.Interceptor;
 import okhttp3.Response;
 import okhttp3.ResponseBody;
 import okio.Buffer;
 import okio.BufferedSource;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 /**
  * {@link ClientResponseFilter} class used to handle rate limit headers.
  */
 class ResponseInterceptor implements Interceptor {
-
-  private static int xRateLimitLimit = -1;
-  private static int xRateLimitRemaining = -1;
-  private static int xRateLimitReset = -1;
-  private static int responseCode = -1;
-
-  @Override
-  public Response intercept(Chain chain) throws IOException {
-    Response response = chain.proceed(chain.request());
-
-    setResponseCode(response.code());
-
-    final String xRateLimitLimit = response.headers().get("X-RateLimit-Limit");
-    if (xRateLimitLimit != null) {
-      setXRateLimitLimit(Integer.parseInt(xRateLimitLimit));
+    
+    private static int xRateLimitLimit = -1;
+    private static int xRateLimitRemaining = -1;
+    private static int xRateLimitReset = -1;
+    private static int responseCode = -1;
+    
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Response response = chain.proceed(chain.request());
+        
+        setResponseCode(response.code());
+        
+        final String xRateLimitLimit = response.headers().get("X-RateLimit-Limit");
+        if (xRateLimitLimit != null) {
+            setXRateLimitLimit(Integer.parseInt(xRateLimitLimit));
+        }
+        
+        final String xRateLimitRemaining = response.headers().get("X-RateLimit-Remaining");
+        if (xRateLimitRemaining != null) {
+            setXRateLimitRemaining(Integer.parseInt(xRateLimitRemaining));
+        }
+        
+        final String xRateLimitReset = response.headers().get("X-RateLimit-Reset");
+        if (xRateLimitReset != null) {
+            setXRateLimitReset(Integer.parseInt(xRateLimitReset));
+        }
+        
+        // Try to handle an error. We have to rely on this method because currently the status codes
+        // returned by the xREL API can't be trusted, e.g. returning 2xx responses for errors.
+        // Otherwise we have an error
+        final ResponseBody body = response.body();
+        if (body == null) {
+            throw new XrelException(getResponseCode());
+        }
+        BufferedSource source = body.source();
+        source.request(Long.MAX_VALUE); // request the entire body.
+        Buffer buffer = source.buffer();
+        // clone buffer before reading from it
+        String responseString = buffer.clone().readString(StandardCharsets.UTF_8);
+        ObjectMapper objectMapper = new ObjectMapper();
+        Error error = null;
+        try {
+            error = objectMapper.readValue(responseString, Error.class);
+        } catch (Exception e) {
+            // Nothing, either there was no error or it was not an xREL API error
+        }
+        if (error != null) {
+            throw new XrelException(error.getErrorDescription(), error, getResponseCode());
+        } else if (!response.isSuccessful()) {
+            throw new XrelException(getResponseCode());
+        }
+        
+        return response;
     }
-
-    final String xRateLimitRemaining = response.headers().get("X-RateLimit-Remaining");
-    if (xRateLimitRemaining != null) {
-      setXRateLimitRemaining(Integer.parseInt(xRateLimitRemaining));
+    
+    /**
+     * Gets the maximum number of requests that the consumer is permitted to make per hour as returned
+     * by the last request. -1 if not yet set.
+     *
+     * @return The X-RateLimit-Limit
+     *
+     * @see <a href="https://www.xrel.to/wiki/2727/api-rate-limiting.html">API: Rate Limiting</a>
+     */
+    public static int getXRateLimitLimit() {
+        return xRateLimitLimit;
     }
-
-    final String xRateLimitReset = response.headers().get("X-RateLimit-Reset");
-    if (xRateLimitReset != null) {
-      setXRateLimitReset(Integer.parseInt(xRateLimitReset));
+    
+    /**
+     * Sets the maximum number of requests that the consumer is permitted to make per hour as returned
+     * by the last request. Use -1 to unset.
+     *
+     * @param xRateLimitLimit The X-RateLimit-Limit to set
+     *
+     * @see <a href="https://www.xrel.to/wiki/2727/api-rate-limiting.html">API: Rate Limiting</a>
+     */
+    private static void setXRateLimitLimit(int xRateLimitLimit) {
+        ResponseInterceptor.xRateLimitLimit = xRateLimitLimit;
     }
-
-    // Try to handle an error. We have to rely on this method because currently the status codes
-    // returned by the xREL API can't be trusted, e.g. returning 2xx responses for errors.
-    // Otherwise we have an error
-    final ResponseBody body = response.body();
-    if (body == null) {
-      throw new XrelException(getResponseCode());
+    
+    /**
+     * Gets the number of requests remaining in the current rate limit window as returned by the last
+     * request. -1 if not yet set.
+     *
+     * @return The X-RateLimit-Remaining
+     *
+     * @see <a href="https://www.xrel.to/wiki/2727/api-rate-limiting.html">API: Rate Limiting</a>
+     */
+    public static int getXRateLimitRemaining() {
+        return xRateLimitRemaining;
     }
-    BufferedSource source = body.source();
-    source.request(Long.MAX_VALUE); // request the entire body.
-    Buffer buffer = source.buffer();
-    // clone buffer before reading from it
-    String responseString = buffer.clone().readString(StandardCharsets.UTF_8);
-    ObjectMapper objectMapper = new ObjectMapper();
-    Error error = null;
-    try {
-      error = objectMapper.readValue(responseString, Error.class);
-    } catch (Exception e) {
-      // Nothing, either there was no error or it was not an xREL API error
+    
+    /**
+     * Sets the number of requests remaining in the current rate limit window as returned by the last
+     * request. Use -1 to unset.
+     *
+     * @param xRateLimitRemaining The X-RateLimit-Remaining to set
+     *
+     * @see <a href="https://www.xrel.to/wiki/2727/api-rate-limiting.html">API: Rate Limiting</a>
+     */
+    private static void setXRateLimitRemaining(int xRateLimitRemaining) {
+        ResponseInterceptor.xRateLimitRemaining = xRateLimitRemaining;
     }
-    if (error != null) {
-      throw new XrelException(error.getErrorDescription(), error, getResponseCode());
-    } else if (!response.isSuccessful()) {
-      throw new XrelException(getResponseCode());
+    
+    /**
+     * Gets the time at which the current rate limit window resets in UTC epoch seconds as returned by
+     * the last request. -1 if not yet set.
+     *
+     * @return The X-RateLimit-Reset
+     *
+     * @see <a href="https://www.xrel.to/wiki/2727/api-rate-limiting.html">API: Rate Limiting</a>
+     */
+    public static int getXRateLimitReset() {
+        return xRateLimitReset;
     }
-
-    return response;
-  }
-
-  /**
-   * Gets the maximum number of requests that the consumer is permitted to make per hour as returned
-   * by the last request. -1 if not yet set.
-   * 
-   * @return The X-RateLimit-Limit
-   * @see <a href="https://www.xrel.to/wiki/2727/api-rate-limiting.html">API: Rate Limiting</a>
-   */
-  public static int getXRateLimitLimit() {
-    return xRateLimitLimit;
-  }
-
-  /**
-   * Sets the maximum number of requests that the consumer is permitted to make per hour as returned
-   * by the last request. Use -1 to unset.
-   * 
-   * @param xRateLimitLimit The X-RateLimit-Limit to set
-   * @see <a href="https://www.xrel.to/wiki/2727/api-rate-limiting.html">API: Rate Limiting</a>
-   */
-  private static void setXRateLimitLimit(int xRateLimitLimit) {
-    ResponseInterceptor.xRateLimitLimit = xRateLimitLimit;
-  }
-
-  /**
-   * Gets the number of requests remaining in the current rate limit window as returned by the last
-   * request. -1 if not yet set.
-   * 
-   * @return The X-RateLimit-Remaining
-   * @see <a href="https://www.xrel.to/wiki/2727/api-rate-limiting.html">API: Rate Limiting</a>
-   */
-  public static int getXRateLimitRemaining() {
-    return xRateLimitRemaining;
-  }
-
-  /**
-   * Sets the number of requests remaining in the current rate limit window as returned by the last
-   * request. Use -1 to unset.
-   * 
-   * @param xRateLimitRemaining The X-RateLimit-Remaining to set
-   * @see <a href="https://www.xrel.to/wiki/2727/api-rate-limiting.html">API: Rate Limiting</a>
-   */
-  private static void setXRateLimitRemaining(int xRateLimitRemaining) {
-    ResponseInterceptor.xRateLimitRemaining = xRateLimitRemaining;
-  }
-
-  /**
-   * Gets the time at which the current rate limit window resets in UTC epoch seconds as returned by
-   * the last request. -1 if not yet set.
-   * 
-   * @return The X-RateLimit-Reset
-   * @see <a href="https://www.xrel.to/wiki/2727/api-rate-limiting.html">API: Rate Limiting</a>
-   */
-  public static int getXRateLimitReset() {
-    return xRateLimitReset;
-  }
-
-  /**
-   * Sets the time at which the current rate limit window resets in UTC epoch seconds as returned by
-   * the last request. Use -1 to unset.
-   * 
-   * @param xRateLimitReset The X-RateLimit-Reset to set
-   * @see <a href="https://www.xrel.to/wiki/2727/api-rate-limiting.html">API: Rate Limiting</a>
-   */
-  private static void setXRateLimitReset(int xRateLimitReset) {
-    ResponseInterceptor.xRateLimitReset = xRateLimitReset;
-  }
-
-  /**
-   * Gets the response code returned by the last request. -1 if not yet set.
-   * 
-   * @return responseCode The response code
-   */
-  public static int getResponseCode() {
-    return responseCode;
-  }
-
-  /**
-   * Sets the response code returned by the last request. Use -1 to unset.
-   * 
-   * @param responseCode The response code to set
-   */
-  private static void setResponseCode(int responseCode) {
-    ResponseInterceptor.responseCode = responseCode;
-  }
-
+    
+    /**
+     * Sets the time at which the current rate limit window resets in UTC epoch seconds as returned by
+     * the last request. Use -1 to unset.
+     *
+     * @param xRateLimitReset The X-RateLimit-Reset to set
+     *
+     * @see <a href="https://www.xrel.to/wiki/2727/api-rate-limiting.html">API: Rate Limiting</a>
+     */
+    private static void setXRateLimitReset(int xRateLimitReset) {
+        ResponseInterceptor.xRateLimitReset = xRateLimitReset;
+    }
+    
+    /**
+     * Gets the response code returned by the last request. -1 if not yet set.
+     *
+     * @return responseCode The response code
+     */
+    public static int getResponseCode() {
+        return responseCode;
+    }
+    
+    /**
+     * Sets the response code returned by the last request. Use -1 to unset.
+     *
+     * @param responseCode The response code to set
+     */
+    private static void setResponseCode(int responseCode) {
+        ResponseInterceptor.responseCode = responseCode;
+    }
+    
 }
